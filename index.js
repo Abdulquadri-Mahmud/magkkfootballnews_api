4c6e3a520177923d9da4acb4cf73d4804ab74215
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-11-15 18:27:12 +0100

    changes made

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "https://adexify-online-shopping-site.vercel.app");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-//     next();
-// });
+app.use(function (req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "https://adexify-online-shopping-site.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    next();
+});
 
 // https://adexify-online-shopping-site.vercel.app/ changes made
 // http://localhost:5173
