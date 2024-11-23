36fa2804b2aff966ca7932a20cac7c318d5454d0
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-11-23 18:02:25 +0100

    changes made on the index file

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://magkfootball.vercel.app");
+    // res.header("Access-Control-Allow-Origin", "https://magkfootball.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     next();
 });
 
