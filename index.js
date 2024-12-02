f5356ae28a5170a154d9292b2f985523b3c455bb
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-12-02 16:19:19 +0100

    changes made

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5174");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
