5c51a13bd7d925a11eb6649059aa5b8e08ca5a73
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-12-04 19:32:06 +0100

    changes made

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use(cors());
 app.use(cookieParser());
 
 const allowedOrigins = [
-    // 'https://magkfootball.vercel.app',
-    // 'https://magkk-football-talk-dashboard.vercel.app',
+    'https://magkfootball.vercel.app',
+    'https://magkk-football-talk-dashboard.vercel.app',
     // 'http://localhost:5173',
-    'http://localhost:5174', 
+    // 'http://localhost:5174', 
 ];
   
 // Configure CORS middleware
