6d959934f79f5efc3d84ce431cb33f7db23652b4
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-12-11 17:12:04 +0100

    changes made

--- controller/userController.js
+++ controller/userController.js
@@ -217,7 +217,7 @@ export const userForgotPassword = async (req, res, next) => {
             subject: 'Password Reset Request',
             text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
             Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
-            http://localhost:5173/user/forgot-password/reset-password/${token}\n\n
+            http://localhost:5173/user/reset-password/${token}\n\n
             If you did not request this, please ignore this email and your password will remain unchanged.\n`
         };
 
